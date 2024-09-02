00fa775f28b8a3dfa4d0db03d7f25425ec2d66eb
devcetechs <dev@cetechs.co>
2024-09-03 02:06:44 +0700

    fix custom layout

--- src/components/CustomLayout.tsx
+++ src/components/CustomLayout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import HeaderComponent from "./HeaderComponent";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function CustomLayout({
   children,
